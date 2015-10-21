74cae7f317e5503a5611dc201d87dd72277a0da2
Manikanta <mani.iiit123@gmail.com>
2015-10-21 12:58:26 +0530

    Feature: add image to expression. EditExpression handler updated
    Summary:
    add image to expression. EditExpression handler updated
    Only jpg/jpeg are supported. Tested on almost 30 clips(added posters).
    
    Test Plan: tested locally. screenshots added
    
    Reviewers: abhilashi, abhimanyu, vamshi
    
    Differential Revision: http://104.155.197.196/D478

--- views/admin/editClip.jsx
+++ views/admin/editClip.jsx
@@ -6,13 +6,14 @@ var React = require('react')
 var InputField = require('../../components/InputField');
 var RedButton = require('../../components/RedButton');
 var Clip = require('../../components/showClip');
-
-
+var config = require('../../config');
+var Autocomplete = require('react-autocomplete');
+var LoadingSpinner = require('./../../components/LoadingSpinner');
 
 module.exports = React.createClass({
 
     getInitialState: function() {
-        return({alert_message: false, voice: []})
+        return({alert_message: false, voice: [], movies: [], actors: [], loading: true})
     },
 
     componentDidMount: function() {
@@ -21,29 +22,140 @@ module.exports = React.createClass({
         function(resp){
                     $('#transcript').val(resp['transcript']);
                     $('#tags').val(resp['tags']);
-                    _this.setState({voice: resp});
+                    $('#language').val(resp['language']);
+                    $('#expression_key').val(resp['key']);
+                    $('#actor_key').val(resp['actor_key']);
+                    $('#movie_key').val(resp['movie_key']);
+                    _this.setState({voice: resp, loading: false});
         });
     },
 
-    handleSubmit: function() {
-        var transcript = $('#transcript').val();
-        var caption = transcript.split(' ').join('-');
-        var tags = $('#tags').val().split(',');
+    /**
+     * [handleKeyUpTags fetchs tags suggestion to implement auto-complete]
+     * @param  {[string]} value [the value of the input field onChange]
+     * @return {[null]}       [sets state]
+     */
+    handleKeyUpTags: function(value) {
+        var _this = this;
+        gapi.client.samosa.api.get_autocomplete_search({value: value}).execute(function(resp) {
+
+            var values = resp['values'];
+ 
+            var value_dict = values.map(function(value) {
+                return {'name': value}
+            })
+
+
+            _this.setState({tags: value_dict});
+        });
+    },
+
+    /**
+     * [handleKeyUpMovies fetchs movies suggestions to implement auto-complete]
+     * @param  {[string]} value [the value of the input field onChange]
+     * @return {[null]}       [sets state]
+     */
+    handleKeyUpMovies: function(value) {
+        var _this = this;
+        gapi.client.samosa.api.get_movie_suggestions({value: value}).execute(function(resp) {
+
+            var values = resp['values'];
+
+            var value_dict = values.map(function(value){
+                return {'name': value}
+            })
+
+            _this.setState({movies: value_dict});
+        });
+    },
+
+    /**
+     * [handleKeyUpActors fetchs actors suggestions to implement auto-complete]
+     * @param  {[string]} value [the value of the input field onChange]
+     * @return {[null]}       [sets state]
+     */
+    handleKeyUpActors: function(value) {
+
         var _this = this;
 
-        console.log(tags);
+        gapi.client.samosa.api.get_actor_suggestions({value: value}).execute(function(resp) {
 
-        gapi.client.samosa.api.update_expression({
-                                                    'id': this.props.params.key,
-                                                    'transcript': transcript,
-                                                    'caption': caption,
-                                                    'tags': tags
-                                            }).execute(
-                                                function(resp) {
-                                                    _this.setState({alert_message: true});
-                                                
-                                            })
-          
+            var values = resp['values'];
+
+            var value_dict = values.map(function(value){
+                return {'name': value}
+            })
+
+            _this.setState({actors: value_dict});
+        });
+    },
+
+    //function to check file size before uploading.
+    beforeSubmit: function(){
+        //check whether browser fully supports all File API
+
+        if (window.File && window.FileReader && window.FileList && window.Blob)
+        {
+        
+            if($('#file').val()) //check empty input filed
+            {
+                var fsize = $('#file')[0].files[0].size; //get file size
+                var ftype = $('#file')[0].files[0].type; // get file type
+        
+                //allow only valid image file types 
+                if(ftype == "image/jpeg" || ftype =="image/jpg") {
+                
+                    //Allowed file size is less than 1 MB (1048576)
+                    if(fsize>1048576) 
+                    {   
+                        $("#output").html("<b>"+fsize +"</b> Too big Image file! <br />Please reduce the size of your photo using an image editor.");
+                        return false
+                    }
+                
+                    $("#output").html("");  
+               
+                }
+                else {
+                    $("#output").html("Please Upload jpg or jpeg format images");
+                    return false;
+                }
+
+            }
+        }
+        else
+        {
+            //Output error to older browsers that do not support HTML5 File API
+            $("#output").html("Please upgrade your browser, because your current browser lacks some new features we need!");
+            return false;
+        }
+    },
+
+    handleSubmit: function() {
+        var _this = this;
+        this.setState({loading: true});
+        var _this = this;
+        var actor = $($(this.refs.actor.getDOMNode()).children()[0]).val();
+        var movie = $($(this.refs.movie.getDOMNode()).children()[0]).val();
+        $('#actor').val(actor);
+        $('#movie').val(movie);
+        var options = { 
+            target:   '#output',
+            beforeSubmit:  _this.beforeSubmit,// target element(s) to be updated with server response 
+            url: config.ajax_url + '/dashboard_post_update_expression',
+            success:    function(response) { 
+                alert(response['status']);
+                _this.setState({loading: false});
+            }, 
+            error: function(response) {
+                $('#alert_message').html(' Notification text is not a localised unicode string. Localise the push notification text.');
+            }
+
+        }; 
+        
+        $('#upload_form').submit(function(e) { 
+            $(this).ajaxSubmit(options);  //Ajax Submit form    
+            return false;
+        });           
     },
 
     render: function() {
@@ -64,10 +176,29 @@ module.exports = React.createClass({
             margin : '10px'
         }
 
+        var inputMovieFieldStyle = {
+            float: 'left',
+            width: '600px',
+            height: 'auto',
+            margin : '10px'
+        }
+
         var submitStyle= {
             width: '100px',
             height: '30px',
             margin: '15px'
+        }
+
+        var topElementsStyle = {
+            float: 'left',
+            margin: '5px',
+        }
+
+        var highlightedItem = {
+            color: 'white',
+            background: 'hsl(200, 50%, 50%)',
+            padding: '2px 6px',
+            cursor: 'default'
         }
 
         var alertStyle = {
@@ -81,9 +212,18 @@ module.exports = React.createClass({
             lineHeight: '45px'
         }
 
+        var actorMovieStyle = {
+            float: 'left'
+        }
+
         if(this.state.alert_message) {
             console.log('alert');
             alertStyle['display'] = 'block'
+        }
+
+        // Adding a loading toast 
+        if (this.state.loading){
+            var loadingSpinner = <LoadingSpinner />
         }
 
         return (
@@ -95,24 +235,89 @@ module.exports = React.createClass({
                         <div id="alert_message" style={alertStyle}>
                                 Clip has been updated !!! 
                         </div>
+                        <form method="post" enctype="multipart/form-data" id="upload_form">
+                                <div style={inputFieldStyle} >
+                                    Transcript
+                                    <InputField name="transcript" id="transcript" placeholder="Transcript" />
+                                </div>
+                                <div style={inputFieldStyle}>
+                                    Tags
+                                    <InputField name="tags[]" id="tags" placeholder="Tags" />
+                                </div> 
+                                <div style={inputFieldStyle}>
+                                    Language
+                                    <InputField name="language" id="language" placeholder="Language" />
+                                </div> 
+                                
+                                <div style={inputFieldStyle} >
+                                    Poster
+                                    <InputField name="file" id="file" type="file" />
+                                </div>
+                                {loadingSpinner}
+                                <div>
+                                    <div style={topElementsStyle}>
+                                    Movies :
+                                      <Autocomplete
+                                            ref="movie"
+                                            getItemValue={(item) => item.name}
+                                            items={this.state.movies}
+                                            onChange={(event, value) => {
+                                                this.handleKeyUpMovies(value);
+                                            }}
+                                            renderItem={(item, isHighlighted) => (
+                                                <div
+                                                style={isHighlighted ? highlightedItem : item}
+                                                >{item.name}</div>
+                                            )}
+                                        />
+                                    </div>
 
-                        <div style={inputFieldStyle} >
-                            Transcript
-                            <InputField id="transcript" placeholder="Transcript" />
+                                    <div style={topElementsStyle}>
+                                    Actors :
+                                   <Autocomplete
+                                            ref="actor"
+                                            getItemValue={(item) => item.name}
+                                            items={this.state.actors}
+                                            onChange={(event, value) => {
+                                                this.handleKeyUpActors(value);
+                                            }}
+                                            renderItem={(item, isHighlighted) => (
+                                                <div
+                                                style={isHighlighted ? highlightedItem : item}
+                                                >{item.name}</div>
+                                            )}
+                                        />
+                                    </div>
+                                </div>
+
+
+                                <div style={inputFieldStyle} >
+                                    <input name="expression_key" id="expression_key" type="hidden" />
+                                </div>
+                                <div style={inputFieldStyle} >
+                                    <input name="actor" id="actor" type="hidden" />
+                                </div>
+                                <div style={inputFieldStyle} >
+                                    <input name="movie" id="movie" type="hidden" />
+                                </div>
+                                <div onClick={this.handleSubmit} style={submitStyle}>
+                                    <RedButton  text = "SUBMIT" />
+                                </div>
+  
+                        </form> 
                         </div>
-                        <div style={inputFieldStyle}>
-                            Tags
-                            <InputField id="tags" placeholder="Tags" />
-                        </div> 
-
-                        <div onClick={this.handleSubmit} style={submitStyle}>
-                            <RedButton  text = "SUBMIT" />
+                        <div style={{float: 'left', marginLeft: '30px'}}>
+                            <Clip data ={this.state.voice} />
+                        </div>   
+                        <div style={{ marginLeft: '30px'}}>
+                            Actor:
+                            <input name="actor_key" id="actor_key" />
                         </div>
-                    </div>
-
-                    <div style={{float: 'left', marginLeft: '30px'}}>
-                        <Clip data ={this.state.voice} />
-                    </div>                        
+                        <div style={{ marginLeft: '30px'}}>
+                            Movie:
+                            <input name="movie_key" id="movie_key" />
+                        </div>    
+                    <div id="output"></div>
                 </div>
         )
     }
